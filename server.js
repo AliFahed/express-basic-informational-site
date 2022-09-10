ac21a780da9887a7a81de6d817d7c4d53bae0c02
Ali Fahed <alifahed67@gmail.com>
2022-09-11 03:53:40 +0800

    update commit

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   console.log("Done");
-  res.send("Hello World!");
+  res.render("index");
 });
 
-app.listen(8000);
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("*", (req, res) => {
+  res.render("404");
+});
+
+app.listen(8080);
